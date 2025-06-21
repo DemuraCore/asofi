07e2ae89e93a09484a5b17e5ce3654a4d701562c
Abdul Vaiz Vahry Iskandar <cyberaioff@gmail.com>
2025-06-21 21:12:39 +0700

    Update minio.go

--- config/minio.go
+++ config/minio.go
@@ -16,13 +16,15 @@ func InitMinio() {
 	accessKeyID := os.Getenv("MINIO_ACCESS")
 	secretAccessKey := os.Getenv("MINIO_SECRET")
 	endpoint := os.Getenv("MINIO_ENDPOINT")
-	// useSSL := true
+	useSSL := true
 
 	var err error
 	MinioClient, err = minio.New(endpoint, &minio.Options{
 		Creds: credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
+	log.Printf("%#v\n", minioClient)
 }
